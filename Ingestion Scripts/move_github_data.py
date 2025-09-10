49c89ca50938a74b5e05d1da65123b1be4a0fdbe
simon-d-balisi <simon.d.balisi@gmail.com>
2025-09-10 16:10:11 -0500

    added the azure upload function

--- Ingestion Scripts/move_github_data.py
+++ Ingestion Scripts/move_github_data.py
@@ -5,9 +5,14 @@ Each season will have a player ID list table and a table with one row per player
 
 Link: https://github.com/vaastav/Fantasy-Premier-League/tree/master
 """
+
+import os
+import dotenv
+import io
+
 import pandas as pd
-import requests
-import json
+
+from azure.storage.blob import BlobServiceClient
 
 
 
@@ -33,14 +38,35 @@ def get_season_gw_file(season: int):
 
 
 
-def upload_to_azure(csv_file: pd.DataFrame):
-    pass
+def upload_to_azure(csv_file: pd.DataFrame, cxn_str: str, azure_container: str, azure_blob_name: str):
+    csv_name = io.StringIO()
+    csv_file.to_csv(csv_name, index=False)
+    csv_string = csv_name.getvalue()
 
+    try:
+        blob_service_clinet = BlobServiceClient.from_connection_string(cxn_str)
+        container_client = blob_service_clinet.get_container_client(azure_container)
+
+        if not container_client.exists():
+            container_client.create_container()
+            print(f'Container "{azure_container}" created.')
+
+        blob_client = container_client.get_blob_client(azure_blob_name)
+        blob_client.upload_blob(csv_string, overwrite=True, content_settings={"content_type": "text/csv"})
+        return True
+
+    except Exception as e:
+        print(f'An error occurred when uploading {azure_blob_name}: {e}')
+        return False
 
 
 def main():
+    dotenv.load_dotenv
+    cxn_str = os.getenv("AZURE_CXN_STRING")
+    container_name = os.getenv("AZURE_CSV_CONTAINER_NAME")
+
     for s in range(START_SEASON, END_SEASON+1):
-        season_tag = f'20{s}-{s+1}'
+        season_tag = f'20{s:02}-{s+1:02}'
         print(f'Moving data for the {season_tag} season')
 
         file = get_season_gw_file(s)
@@ -48,5 +74,6 @@ def main():
             print('Data loading unsuccessful')
             continue
 
-        upload_to_azure(file)
+        azure_blob_name = f'player_gw_raw__20{s}_{s+1}.csv'
+        upload_to_azure(file, cxn_str, container_name, azure_blob_name)
 
