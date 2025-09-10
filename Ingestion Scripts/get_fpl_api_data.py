6d2746838f248727b17fda2dbea51e3b4f50904b
simon-d-balisi <simon.d.balisi@gmail.com>
2025-09-10 09:54:32 -0500

    Adding placeholder files for the data ingestion scripts

---
+++ Ingestion Scripts/get_fpl_api_data.py
@@ -0,0 +1,5 @@
+"""
+This file will be for parsing through the fpl API and getting all data for the current season and matchweek.
+
+It will upload the raw data directly into azure blob storage.
+"""
---
+++ Ingestion Scripts/move_github_data.py
@@ -0,0 +1,7 @@
+"""
+This file will be for reading data directly from the fpl data github by vaastav (link below) directly into Azure blob storage.
+
+Each season will have a player ID list table and a table with one row per player-matchweek.
+
+Link: https://github.com/vaastav/Fantasy-Premier-League/tree/master
+"""
