3e52c84f7375c76b8d0d7b00762062e601517f3a
simon-d-balisi <simon.d.balisi@gmail.com>
2025-09-10 10:25:11 -0500

    Added main method to read from github data

--- Ingestion Scripts/move_github_data.py
+++ Ingestion Scripts/move_github_data.py
@@ -5,3 +5,48 @@ Each season will have a player ID list table and a table with one row per player
 
 Link: https://github.com/vaastav/Fantasy-Premier-League/tree/master
 """
+import pandas as pd
+import requests
+import json
+
+
+
+
+START_SEASON = 15 # beginning of first season of available data (representing 2015-16)
+END_SEASON = 24 # beginning of final season of available data (representing 2024-25)
+
+
+
+def get_season_gw_file(season: int):
+    season_tag = f'20{season}-{season+1}'
+    url = f"https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/{season_tag}/gws/merged_gw.csv"
+
+    try:
+        csv_file = pd.read_csv(url)
+
+    except Exception as e:
+        print(f'Exception occurred: {e}')
+        return None
+
+    else:
+        return csv_file
+
+
+
+def upload_to_azure(csv_file: pd.DataFrame):
+    pass
+
+
+
+def main():
+    for s in range(START_SEASON, END_SEASON+1):
+        season_tag = f'20{s}-{s+1}'
+        print(f'Moving data for the {season_tag} season')
+
+        file = get_season_gw_file(s)
+        if file is None:
+            print('Data loading unsuccessful')
+            continue
+
+        upload_to_azure(file)
+
